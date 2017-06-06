37b6183643d2c3c36a878147e63227c67b408d8e
HoneyBadgered <honeybadgeredhoney@gmail.com>
2017-06-06 15:25:26 +0000

    Initial Commit

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
 
-phpinfo();
+$conn = new mysqli($servername,$username, $password);
 
+if($conn->connect_error){
+		die("Connection failed:" . $conn->connect_error);
+}
+
+echo "Successful Connection";
+/*<html>
+<form action="" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+<input type="submit">
+    
+</form>
+</html>
+*/
 ?>
